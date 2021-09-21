1d19d955db665f467092ca9d2238ca584a5e29f2
Kirillova Olesya <olesyak-85@mail.ru>
2021-09-21 22:43:39 +0300

    added classes Movie and MovieManager, failIfNoTests

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,58 @@
+package ru.netology.domain;
+
+public class Movie {
+    private int id;
+    private int movieId;
+    private String name;
+    private String genre;
+    private String imageUrl;
+
+    public Movie(int id, int movieId, String name, String genre, String imageUrl) {
+        this.id = id;
+        this.movieId = movieId;
+        this.name = name;
+        this.genre = genre;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
+
---
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -0,0 +1,41 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] items = new Movie[0];
+    private int limitMovieToGet = 10;
+
+    public MovieManager() {
+    }
+
+    public MovieManager(int limitMovieToGet) {
+        this.limitMovieToGet = limitMovieToGet;
+    }
+
+    public void add(Movie item) {
+        int length = items.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public Movie[] getAll() {
+        int resultLength;
+        if (items.length > limitMovieToGet) {
+            resultLength = limitMovieToGet;
+        } else {
+            resultLength = items.length;
+        }
+        Movie[] result = new Movie[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = resultLength - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+
+}
